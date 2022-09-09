10b7954da7add3beb6ecb5aff2a7d393d3a06959
Brent Wodicka <brent.wodicka@appliedis.com>
2022-09-09 11:39:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Pulls JSON data into pandas dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# outputs the contents of the pandas dataframe into a streanlit table
+streamlit.dataframe(fruityvice_normalized)
